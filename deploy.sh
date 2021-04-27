25e68ffc71082ad3538413c5cd6eabb62bb9e01d
风筝 <13286953452@126.com>
2021-04-27 22:49:17 +0800

    add deploy shell

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+if ! [ -x "$(command -v git)" ]; then
+    echo 'Error: git is not installed.' >&2
+    exit 1
+fi
+
+if [[ -z $(git status -s) ]]; then
+    echo "tree is clean"
+else
+    echo -e "\033[36;1m tree is dirty, please commit changes before running this \033[0m"
+    exit 1
+fi
+
+version="$(git describe --exact-match --tags HEAD)"
+branch="$(git rev-parse --abbrev-ref HEAD)"
+
+if [ -z "$version" ]; then
+    version=$branch-"$(date +'%Y-%m-%d-%H%M%S')"
+    echo "making new version " $version
+else
+    echo "using old version " $version
+fi
+
+echo "tagging " $version
+
+git tag $version
+
+d="$(git remote -v)"
+depository=($(echo $d | tr ',' ' '))
+echo -e "Found depository: "$depository""
+if [ -z $depository ]; then
+    echo -e "match depository"
+    depository="origin"
+fi
+
+echo "git push tag"
+git push $depository $version
+
+npm publish
+
+echo -e "\033[46;30m exec done \033[0m"
