296257df1f31f4d4aff0b9188057f16efc959ef4
风筝 <13286953452@126.com>
2021-05-11 09:36:45 +0800

    add version notifier

--- j.js
+++ j.js
@@ -2,9 +2,8 @@
 'use strict';
 
 const semver = require('semver');
-
-const packageJson = require('./package.json');
-
+const path = require('path');
+const packageJson = require(path.resolve(__dirname, 'package.json'));
 const currentNodeVersion = process.version.replace('v', '');
 const requiresNodeVersion = packageJson.engines.node;
 
@@ -16,5 +15,20 @@ if (!semver.satisfies(currentNodeVersion, requiresNodeVersion)) {
     process.exit(1);
 }
 
+
 const run = require('./index');
 run(process, process.cwd(), __dirname);
+
+
+const args = process.argv.slice(2);
+// version notifier
+if (args.indexOf("--quiet") === -1) {
+    const updateNotifier = require("update-notifier");
+    // eslint-disable-next-line security/detect-non-literal-require
+    const notifier = updateNotifier({ pkg: packageJson });
+    notifier.notify();
+}
+
+
+
+
