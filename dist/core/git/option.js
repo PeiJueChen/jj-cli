a2d0a4da548a13ca0ca2e1cb31fbe7f70ec8b184
Peijue Chen <peijue.chen@aigens.cn>
2021-09-11 17:20:49 +0800

    preview support changed files auto refresh browser

--- dist/core/git/option.js
+++ dist/core/git/option.js
@@ -1,1 +1,1 @@
-"use strict";const e=require("commander"),o=()=>{e.option("-m --message <message>","Commit message"),e.option("-b --branch <branch>","Set your branch; Default is 'current'"),e.option("-d --depository <depository>","Set your depository, Default is: orgin")};module.exports=o;
+"use strict";const e=require("commander"),o=()=>{e.option("-m --message <message>","Commit message"),e.option("-b --branch <branch>","Set your branch; Default is 'current'"),e.option("-d --depository <depository>","Set your depository, Default is: origin")};module.exports=o;
--- updatelog.md
+++ updatelog.md
@@ -1,5 +1,5 @@
 ## update log
-
+* 0.7.0  -- preview, use live-server, support changed files auto refresh browser.
 * 0.6.2  -- fixed: get ip, flatmap
 * 0.6.1  -- use gulp
 * 0.6.0  -- add preview & get ip
